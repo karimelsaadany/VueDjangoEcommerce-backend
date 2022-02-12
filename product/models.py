7ec2a928098d10ba1f5faf336681ebfcd034b52c
Karim Elsaadany <kasaadany@gmail.com>
2022-02-12 16:31:36 +0200

    create view for product details

--- product/models.py
+++ product/models.py
@@ -14,6 +14,9 @@ class Category(models.Model):
 
   def __str__(self):
     return self.name
+  
+  def get_absolute_url(self):
+    return f'/{self.slug}/'
 
 class Product(models.Model):
   category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
@@ -30,6 +33,9 @@ class Product(models.Model):
 
   def __str__(self):
     return self.name
+  
+  def get_absolute_url(self):
+    return f'/{self.category.slug}/{self.slug}/'
   
   def get_image(self):
     if self.image:
--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views import LatestProductsList
+from .views import ProductDetail
 
 urlpatterns = [
-  path('latest-products/', LatestProductsList.as_view())
+  path('latest-products/', LatestProductsList.as_view()),
+  path('products/<slug:category_slug>/<slug:product_slug>', ProductDetail.as_view()),
 ]
